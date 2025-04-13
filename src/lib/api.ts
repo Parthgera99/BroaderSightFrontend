f909c87b47e5a20513584b14fb7da0a9a3108395
Parth Gera <parthgera2004@gmail.com>
2025-04-13 13:07:28 +0530

    baseURL changed

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const baseURL =   process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1";
+
 // ✅ Define the base API instance
 const api = axios.create({
-  baseURL: "http://192.168.29.237:8000/api/v1", 
+  baseURL, 
   withCredentials: true, // Needed for cookies (authentication)
   headers: {
     "Content-Type": "application/json",
