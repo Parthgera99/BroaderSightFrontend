1c39cd696baf7f607f970b47f6c59623335f0538
Parth Gera <parthgera2004@gmail.com>
2025-04-30 13:26:31 +0530

    updates category fetch for cache

--- src/lib/categoryService.ts
+++ src/lib/categoryService.ts
@@ -2,14 +2,22 @@ import api from "@/lib/api";
 import { AxiosError } from "axios";
 
 export async function getCategories() {
-    try {
-      console.log("Fetching categories...");
-      const response = await api.get("/category/list");
-      console.log("Categories fetched");
-      return response.data.data;  
-    } catch (error) {
-      const err = error as AxiosError;
-      console.error("Error fetching categories:", err.response?.data || err.message);
-      return [];  
+  try {
+    console.log("Fetching categories...");
+
+    const res = await fetch("http://192.168.224.48:8000/api/v1/category/list", {
+      next: { revalidate: 60 }, // cache for 60 seconds
+    });
+
+    if (!res.ok) {
+      throw new Error(`Fetch failed with status ${res.status}`);
     }
+
+    const json = await res.json();
+    console.log("Categories fetched");
+    return json.data;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return [];
   }
+}
--- src/lib/fetchBlogList.ts
+++ src/lib/fetchBlogList.ts
@@ -54,24 +54,29 @@ export async function fetch3BlogList(): Promise<Blog[]> {
 
   export async function fetchCategory3BlogsList(category: string): Promise<Blog[]> {
     try {
-        const response = await api.post(`/blog/category/${category}?type=trending&limit=3`, {
-            headers: {
-                "Cache-Control": "no-cache, no-store, must-revalidate",
-                Pragma: "no-cache",
-                Expires: "0",
-            },
-        });
-
-        // console.log(response.data.data.blogs)
-    
-        // ✅ Axios automatically parses JSON, so no need for `.json()`
-        if (!response.data.data || !response.data.data.blogs) {
-        throw new Error("Failed to fetch blogs");
-        }
-        
-        return response.data.data.blogs;
-    } catch (error) {
-        // console.error("Error fetching blogs:", error);
+      const res = await fetch(`http://192.168.224.48:8000/api/v1/blog/category/${category}?type=trending&limit=3`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        next: { revalidate: 60 } 
+      });
+  
+      if (!res.ok) {
+        console.log(`Failed to fetch ${category} blogs`);
         return [];
+      }
+  
+      const json = await res.json();
+  
+      if (!json?.data?.blogs) {
+        throw new Error("No blogs found in response");
+      }
+  
+      return json.data.blogs;
+    } catch (error) {
+      console.error("Error fetching blogs:", error);
+      return [];
     }
   }
+  
